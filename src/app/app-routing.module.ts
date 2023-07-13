1fd21e9e6e8ecffc46c8740c0774724f7c424db5
meef3k <mateuszjawor99@gmail.com>
2023-07-13 16:10:33 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EpicListComponent } from './epic-list/epic-list.component';
+import { EpicCreateComponent } from './epic-create/epic-create.component';
+import { EpicDetailComponent } from './epic-detail/epic-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'epics', pathMatch: 'full'},
+  {path: 'epics', component: EpicListComponent},
+  {path: 'epics/create', component: EpicCreateComponent},
+  {path: 'epics/:id', component: EpicDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
